698f6fe4dab262b27ae1425f876e1108464ec98b
Andrii Saichuk <saychuk.andriy@gmail.com>
2022-02-21 01:09:27 +0200

    build prod js.maps

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./app";
 import { Buffer } from "buffer";
-import "../src/assets/styles/index.css";
+import "./assets/styles/index.css";
 
 // it prevents react to raise an error with Buffer size
 // when we try to use window object
